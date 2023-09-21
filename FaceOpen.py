239ff05db4c76cbce9a1a1e1f34ec70f0a23fc32
Enes Eken <144843964+eneseken95@users.noreply.github.com>
2023-09-21 17:03:22 +0300

    V1

---
+++ FaceOpen.py
@@ -0,0 +1,13 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(r"haarcascade_frontalface_default.xml")
+img=cv2.imread(r"foto.jpeg")
+gray=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+faces = face_cascade.detectMultiScale(gray,1.1,4)
+
+for(x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w, y+h),(0,255,0),3)
+    
+cv2.imshow('img',img)
+cv2.waitKey(0)
